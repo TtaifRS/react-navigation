33eeaf89739719961703e2fefbe375400da001a1
Taif Rahaman <taif.rahaman73@gmail.com>
2021-05-03 16:45:36 +0600

    basic impliment of stack navigator with styling

--- App.js
+++ App.js
@@ -1,26 +1,71 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-const HomeScreen = () => {
+const Stack = createStackNavigator();
+
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Welcome to React Native Navigation</Text>
+      <Text style={{ marginBottom: 10 }}>
+        Welcome to React Native Navigation
+      </Text>
+      <Button
+        title="Details Screen"
+        onPress={() => navigation.navigate('Details')}
+      />
       <StatusBar style="auto" />
     </View>
   );
 };
 
-const Stack = createStackNavigator();
+const DetailScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>DetailS Screen</Text>
+      <StatusBar style="auto" />
+      <View style={{ margin: 10 }}>
+        <Button
+          title="Details Screen again"
+          onPress={() => navigation.push('Details')}
+        />
+        <Button
+          title="Home Screen"
+          onPress={() => navigation.navigate('Home')}
+        />
+        <Button title="Back Screen" onPress={() => navigation.goBack()} />
+        <Button
+          title="Go to the first screen"
+          onPress={() => navigation.popToTop()}
+        />
+      </View>
+    </View>
+  );
+};
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#0579b3f0',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Main Page' }}
+        />
+        <Stack.Screen name="Details" component={DetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
